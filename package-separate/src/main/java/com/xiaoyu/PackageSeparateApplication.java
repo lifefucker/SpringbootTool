d975026a395b8c05a3c1a8d7cb183722227da8c6
lmy <1373521920@qq.com>
2020-12-13 21:03:53 +0800

    springboot分离配置和lib

---
+++ package-separate/src/main/java/com/xiaoyu/PackageSeparateApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageSeparateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageSeparateApplication.class, args);
+	}
+
+}
